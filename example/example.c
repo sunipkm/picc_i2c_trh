599dd2b054f48669a4e3aab91798582637032860
Sunip Mukherjee <sunipkmukherjee@gmail.com>
2018-06-01 14:26:41 -0400

    Beta2

--- example/example.c
+++ example/example.c
@@ -14,16 +14,16 @@ int main ( void )
 	printf ( "Device 2:\nManufacturer ID: 0x%x,\n" , hdc1010_readMfId(dev2) ) ;
 	printf ( "Device ID: 0x%x.\n\n" , hdc1010_readDevId(dev2)) ;
 
-	uint64_t time = 0 ;
+	int time = 0 ;
 
-	printf ( "Acquisition Mode: Independent\n" ) ;
+	printf ( "Acquisition Mode: Independent\n\n" ) ;
 	while(1)
 	{
 		printf ( "At %d seconds:\n" , time ) ;
 		printf ( "Temperature from Device 1: %.3f C\n" , hdc1010_readT(dev1)) ;
 		printf ( "Temperature from Device 2: %.3f C\n\n" , hdc1010_readT(dev2)) ;
 		printf ( "Relative Humidity from Device 1: %.3f %\n" , hdc1010_readH(dev1)) ;
-		printf ( "Relative Humidity from Device 2: %.3f %\n\n" , hdc1010_readH(dev2)) ;
+		printf ( "Relative Humidity from Device 2: %.3f %\n\n\n" , hdc1010_readH(dev2)) ;
 		time += 10 ;
 		if ( time > 20 )
 			break ;
