a03d38dc99900663bdb3030773912eb93d82f256
Sunip Mukherjee <sunipkmukherjee@gmail.com>
2018-06-01 14:32:03 -0400

    Beta2

--- example/example.c
+++ example/example.c
@@ -32,5 +32,27 @@ int main ( void )
 			printf("\x1b[A") ;
 	}
 
+	printf ( "Acquisition mode: Simultaneous\n\n")
+
+	time = 0 ;
+
+	hdc1010_acquisition_mode ( dev1 , 0 ) ;
+	hdc1010_acquisition_mode ( dev2 , 0 ) ;
+
+	while(1)
+	{
+		printf ( "At %d seconds:\n" , time ) ;
+		printf ( "Temperature from Device 1: %.3f C\n" , hdc1010_readT(dev1)) ;
+		printf ( "Temperature from Device 2: %.3f C\n\n" , hdc1010_readT(dev2)) ;
+		printf ( "Relative Humidity from Device 1: %.3f %\n" , hdc1010_readH(dev1)) ;
+		printf ( "Relative Humidity from Device 2: %.3f %\n\n\n" , hdc1010_readH(dev2)) ;
+		time += 10 ;
+		if ( time > 20 )
+			break ;
+		hdc1010_sleep(10000) ; //spit out numbers in 1 second intervals
+		for ( uint8_t i = 0 ; i < 8 ; i++ )
+			printf("\x1b[A") ;
+	}
+
 	hdc1010_free(dev1) ; hdc1010_free(dev2) ;
 }
